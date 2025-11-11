7605d19c87dbb8de534467665b56c452b7a862f2
Zakaria El Yamani <zel-yama@e1r10p2.1337.ma>
2025-11-11 11:33:32 +0100

    start of cpp 09

---
+++ Module09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,12 @@
+ #ifndef BITCOINEXCHANGE_HPP
+ #define BITCOINEXCHANGE_HPP
+#include <iostream>
+#include <map>
+class BitcoinExchange{
+    private:
+        std::map<long, int> map;
+    
+        
+};
+
+ #endif
---
+++ Module09/ex00/main.cpp
@@ -0,0 +1,6 @@
+
+int main(int av, char *ac[])
+{
+    if (av != 2)
+        return (1); 
+}
