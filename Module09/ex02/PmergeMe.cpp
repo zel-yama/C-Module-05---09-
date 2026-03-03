74acf213ff04980303d4cf89fd3287c8a81b933d
Zakaria El Yamani <zel-yama@e2r9p4.1337.ma>
2026-03-03 00:01:57 +0000

    some word create

--- Module09/ex02/PmergeMe.cpp
+++ Module09/ex02/PmergeMe.cpp
@@ -1,6 +1,13 @@
 
 
 #include "PmergeMe.hpp"
+//  pair and return winners be shore that winner can insert abel 
+//  recursion untill size equel tow check if value upaired put value number unpaired var 
+//  when recursion reach base case return back with reucrsion  
+//
+//
+//
+//
 
 int convertString(std::string str){
     
@@ -24,32 +31,59 @@ void printDebug(single v){
     }
     printf("\n-----------------\n");
 }
-void printDouble(doubleV v){
-    doubleV::iterator it = v.begin();
-    while(it != v.end()){
-        printf("this is first -> %d -- seconde %d\n", it->first, it->second);
-        it++;
-    }
-}
-void pushValue(int node1, int node2, PmergeMe &obj){
 
-    if (node1 < node2)
-        obj.V2.push_back(std::make_pair(node1, node2));
-    else
-        obj.V2.push_back(std::make_pair(node2, node1));
+
+std::vector<int> JacobsthalSequenec(int n){
     
-    
+    std::vector<int> v;
+    if (n <= 0)
+        return v;
+    v.push_back(0);
+    if (n == 1){
+        
+        return v;
+    }   
+    v.push_back(1);
+    int i  = 2;
+    while (i < n)
+    {
+        size_t val = v[i - 1]  + 2 * v[i - 2];
+        v.push_back(val);
+        i++;
+    }
+    return v;
 }
 
-void CreateDoubleVector(PmergeMe &obj){
-    if (obj.V1.size() % 2 == 1){
-        obj.upairedValue = obj.V1.back();
-        obj.V1.erase(obj.V1.end() - 1 , obj.V1.end());
-         printDebug(obj.V1);
+
+void mergeInsertion(PmergeMe &obj, single v){
+
+    if (obj.V1.size() <= 2)
+        return;
+    std::deque<int> losers;
+    std::deque<int> winner;
+    int unpaired ;
+    int size = obj.V1.size() -1;
+    if (obj.V1.size() % 2 != 0){
+        unpaired = obj.V1[size];
+        size--;
     }
-    for(size_t i = 0;  (i + 1) < obj.V1.size(); i+=2){
-        pushValue(obj.V1[i], obj.V1[i + 1], obj);
+
+    int i = 0;
+    while (i < size)
+    {
+        if (obj.V1[i] > obj.V1[i + 1] ){
+            winner.push_back(obj.V1[ i + 1]);
+            losers.push_back(obj.V1[i]);
+        }
+        else{
+           losers.push_back(obj.V1[ i + 1]);
+            winner.push_back(obj.V1[i]);
+        }
+        i+=2;
     }
+    mergeInsertion(obj, obj.V1);
+    printf("unpaired Value %d \n", unpaired);
+    printf("winner\n");
 }
 
 void parsing(int ac, char *av[], PmergeMe &obj){
@@ -66,8 +100,9 @@ void parsing(int ac, char *av[], PmergeMe &obj){
             else    
                 obj.V1.push_back(local);
         }
-        printDebug(obj.V1);
-       CreateDoubleVector(obj);
-       printDouble(obj.V2);
+        //printDebug(obj.V1);
+        mergeInsertion(obj, obj.V1);
+        //CreateDoubleVector(obj);
+    
     }
 }
--- Module09/ex02/PmergeMe.hpp
+++ Module09/ex02/PmergeMe.hpp
@@ -5,16 +5,17 @@
 #include <iostream>
 #include <exception> 
 #include <sstream>
+#include <deque>
 typedef std::vector<int> single;
-typedef std::vector<std::pair<int, int> >  doubleV;
+
 
 class PmergeMe{
 
     public:
         int upairedValue;
-        doubleV V2;
+       
         single V1; 
-        
+        std::deque<int> DEQ;
 };
 void parsing(int ac, char *av[], PmergeMe &obj);
 
