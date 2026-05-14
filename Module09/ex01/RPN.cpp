88ad3ba619116d6da9b6af48129343ff64550a0c
Zakaria El Yamani <zel-yama@e1r6p6.1337.ma>
2026-05-14 10:52:18 +0100

    just fix parsing

--- Module09/ex01/RPN.cpp
+++ Module09/ex01/RPN.cpp
@@ -15,31 +15,32 @@ RPN &RPN::operator=(const RPN &obj){
     (void)obj;
     return *this;
 }
-int switchCases(int num, int num1, char c){
+int switchCases(long num, long num1, char c){
     
-    int res = 0;
+    long long res = 0;
     if (num1 == 0 && c == '/')
-        throw std::runtime_error("Error division by ziro is not allowed ");
+        throw std::runtime_error("Error division by zero is not allowed ");
     
     switch (c)
     {
-    case '*':
-        res = num * num1;
-        break;
-    case '/':
-        
-        res = num / num1;
-        break;
-    case '+':
-        res = num + num1;
-        break;
-    case '-':
-        res = num - num1;
-        break;
-    default:
-        throw std::runtime_error("Error : invalid operation {/*+-}");
-        break;
+        case '*':
+            res = num * num1;
+            break;
+        case '/':
+            res = num / num1;
+            break;
+        case '+':
+            res = num + num1;
+            break;
+        case '-':
+            res = num - num1;
+            break;
+        default:
+            throw std::runtime_error("Error : invalid operation {/*+-}");
+            break;
     }
+    if (res > 2147483647)
+        throw std::runtime_error("Error: the max value exceeded max int");
     return res;
 }
 void caloperation(std::stack<int> &stak, char c){
@@ -68,15 +69,14 @@ void RPN::operations(std::string av){
 
     for(size_t i = 0; i < size ; i++){
         
-
+        if (!std::isdigit(av[i]) && av[i] != '/' && av[i] != '+' && av[i] != '*' && av[i] != '-' && av[i] != ' '  )
+            throw std::runtime_error("invalid character in => " + av );
         if (std::isdigit((unsigned int )av[i])){
             if ((i + 1 < size) && std::isdigit(av[i + 1])) {
                 std::cout << "Error : in number should be < 10   " << std::endl;
                 return ;
             }
-
             mystack.push((int)av[i] - '0');
-          
         }
         else if (std::isspace(av[i]))
             continue;
@@ -85,7 +85,12 @@ void RPN::operations(std::string av){
             caloperation(mystack,  av[i]);
         }
     }
-    if (mystack.size() > 1){
+    if ( mystack.empty()){
+        std::cout << "Error: empty stack " << std::endl;
+        return ;
+    }
+
+    if (mystack.size() > 1 ){
         std::cout << "Error: invalid insertion of operation " << std::endl;
         return;
     }
